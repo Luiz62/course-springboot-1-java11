557112547aed6d06fb1038bb137c23627f4230fc
Luiz62 <luiztiagonascimento21@gmail.com>
2021-12-30 22:24:59 -0300

    Update OrderItem, many-to-many association with extra attributes

---
+++ src/main/java/com/example/course/services/OrderItemService.java
@@ -0,0 +1,24 @@
+package com.example.course.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.course.entities.OrderItem;
+import com.example.course.repositories.OrderItemRepository;
+
+@Service
+public class OrderItemService {
+
+	@Autowired
+	private OrderItemRepository repository;
+	
+	public List<OrderItem> findAll() {
+		return repository.findAll();
+	}
+	
+	public OrderItem findById(Long id) {
+		return repository.findById(id).get();
+	}
+}
